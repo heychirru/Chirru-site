eb9098fee5726e5ffaf3861fb87a8e5452922880
Chiranjit Das <147528779+heychirru@users.noreply.github.com>
2026-04-28 22:44:50 +0530

    Add Spring Boot main application

---
+++ Backend-Java/src/main/java/com/chirru/ChirruApplication.java
@@ -0,0 +1,11 @@
+package com.chirru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChirruApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChirruApplication.class, args);
+    }
+}
